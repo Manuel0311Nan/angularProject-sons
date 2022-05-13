64e3b24f9bc2140bfcc2d68820edc614dcacc555
Manuel Alcázar <manuzz3112@gmail.com>
2022-05-13 18:52:27 +0200

    header & footer to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './core/components/header/header.component';
 import { ListComponent } from './list/list.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from '../app/core/components/footer/footer.component';
 import { CharacterComponent } from './list/character/character.component';
-import { SocialMediaComponent } from './footer/social-media/social-media.component';
+import { SocialMediaComponent } from '../app/core/components/footer/social-media/social-media.component';
 
 @NgModule({
   declarations: [
